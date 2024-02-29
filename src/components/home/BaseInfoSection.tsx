3f8b0b9d5e0f879fd8796a3c4019cd2f4d4f1983
Filip Tomes <filiptomes@protonmail.com>
2024-02-29 10:00:12 +0100

    syle: replace video by youtube iframe

--- src/components/home/BaseInfoSection.tsx
+++ src/components/home/BaseInfoSection.tsx
@@ -1,11 +1,10 @@
-import video from '../../assets/videos/majales-2022.mp4';
 import { Container } from '../shared/Container';
 
 export const BaseInfoSection = () => {
   return (
     <section id="zakladni-informae" className="bg-purple">
       <Container>
-        <div className="flex flex-wrap gap-8 xl:gap-12 w-full justify-between py-8 xl:py-16 xl:text-p-big">
+        <div className="flex flex-wrap gap-8 xl:gap-12 w-full justify-between py-8 xl:py-16 xl:text-p-big items-center">
           <div className="w-[492px] space-y-2">
             <p>
               Vstup <span className="font-semibold">ZDARMA</span> pro všechny
@@ -16,10 +15,15 @@ export const BaseInfoSection = () => {
             <p>Král a Královna & Princ a Princezna Majálesu</p>
             <p>To a mnohem víc na Majálesu UTB!</p>
           </div>
-          <video width={685} height={380} className="bg-beige rounded-2xl text-blue" autoPlay muted controls>
-            <source src={video} type="video/mp4" />
-            video z roku 2022
-          </video>
+          <iframe
+            width="685"
+            height="380"
+            src="https://www.youtube.com/embed/ao12Gek8WRg?si=N95euryS4_R8r7gT"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+            className="bg-beige rounded-2xl"
+          />
         </div>
       </Container>
     </section>
