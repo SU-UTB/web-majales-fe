2e7ad2d2e03f87f5a215915dc91c76b098bbe0ac
Filip Tomes <60915718+tomesfilip@users.noreply.github.com>
2024-03-02 13:47:24 +0100

    Feat/lineup (#13)
    * style: fix cut out nav logo icon
    
    * refactor: new section container reusable component
    
    * style: basic content and section preparation
    
    * refactoring: colors type definition, refactore home page components

--- src/components/home/BaseInfoSection.tsx
+++ src/components/home/BaseInfoSection.tsx
@@ -1,31 +1,29 @@
-import { Container } from '../shared/Container';
+import { SectionContainer } from '../shared/SectionContainer';
 
 export const BaseInfoSection = () => {
   return (
-    <section id="zakladni-informae" className="bg-purple">
-      <Container>
-        <div className="flex flex-wrap gap-8 xl:gap-12 w-full justify-between py-8 xl:py-16 xl:text-p-big items-center">
-          <div className="w-full max-w-[492px] space-y-2">
-            <p>
-              Vstup <span className="font-semibold">ZDARMA</span> pro všechny
-            </p>
-            <p>2 stage se 7 interprety </p>
-            <p>Food & Chill-out zóna</p>
-            <p>Doprovodný program</p>
-            <p>Král a Královna & Princ a Princezna Majálesu</p>
-            <p>To a mnohem víc na Majálesu UTB!</p>
-          </div>
-          <iframe
-            width="685"
-            height="380"
-            src="https://www.youtube.com/embed/ao12Gek8WRg?si=N95euryS4_R8r7gT"
-            title="YouTube video player"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowFullScreen
-            className="bg-beige rounded-2xl mx-auto"
-          />
+    <SectionContainer id="zakladni-informae" bgColor="purple">
+      <div className="flex flex-wrap gap-8 xl:gap-12 w-full justify-between py-8 xl:py-16 xl:text-p-big items-center">
+        <div className="w-full max-w-[492px] space-y-2">
+          <p>
+            Vstup <span className="font-semibold">ZDARMA</span> pro všechny
+          </p>
+          <p>2 stage se 7 interprety </p>
+          <p>Food & Chill-out zóna</p>
+          <p>Doprovodný program</p>
+          <p>Král a Královna & Princ a Princezna Majálesu</p>
+          <p>To a mnohem víc na Majálesu UTB!</p>
         </div>
-      </Container>
-    </section>
+        <iframe
+          width="685"
+          height="380"
+          src="https://www.youtube.com/embed/ao12Gek8WRg?si=N95euryS4_R8r7gT"
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+          className="bg-beige rounded-2xl mx-auto"
+        />
+      </div>
+    </SectionContainer>
   );
 };
--- src/components/home/LinkSection.tsx
+++ src/components/home/LinkSection.tsx
@@ -1,18 +1,16 @@
 import { usefulLinks } from '../../data/usefulLinks';
-import { Container } from '../shared/Container';
+import { SectionContainer } from '../shared/SectionContainer';
 import { LinkCard } from './usefulLinks/LinkCard';
 
 export const LinkSection = () => {
   return (
-    <section id="mohlo-by-se-hodit">
-      <Container>
-        <h2 className="text-orange text-h2-reversed">Mohlo by se vám hodit</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-x-4 gap-y-16 py-8 content-center">
-          {usefulLinks.map((link) => (
-            <LinkCard {...link} />
-          ))}
-        </div>
-      </Container>
-    </section>
+    <SectionContainer id="mohlo-by-se-hodit">
+      <h2 className="text-orange text-h2-reversed">Mohlo by se vám hodit</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-x-4 gap-y-16 py-8 content-center">
+        {usefulLinks.map((link) => (
+          <LinkCard {...link} />
+        ))}
+      </div>
+    </SectionContainer>
   );
 };
