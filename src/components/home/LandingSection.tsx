a881fb0e2eb54dc84cd363404cc6b056b21890ef
Filip Tomes <filiptomes@protonmail.com>
2024-03-03 20:26:35 +0100

    fix: main page logo width

--- src/components/home/LandingSection.tsx
+++ src/components/home/LandingSection.tsx
@@ -9,9 +9,9 @@ export const LandingSection = () => {
   return (
     <section id="landing" className="py-12 xl:py-20">
       <Container>
-        <div className="relative mx-auto max-w-max">
+        <div className="relative flex justify-center">
           <MajalesHeadline width={706} />
-          <div className="absolute z-10 top-[80%]">
+          <div className="absolute z-10 top-[80%] left-[10%]">
             <HeadlineBubble fillColor="#4400FF" rotationValue={10}>
               <h2 className="text-h2 text-beige">{MAJALES_DATE.format('D. M. YYYY')}</h2>
             </HeadlineBubble>
