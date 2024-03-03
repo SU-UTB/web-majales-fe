d8bef92df6e626173d31e6f5fd61d22e6f2cc4fc
Filip Tomes <filiptomes@protonmail.com>
2024-03-03 22:44:21 +0100

    style: partneri section

---
+++ src/components/partners/PartnerList.tsx
@@ -0,0 +1,8 @@
+export const PartnerList = () => {
+  return (
+    <div className="space-y-8">
+      <h4 className="text-blue text-h2 text-center">Partneři</h4>
+      <div className="rounded-2xl bg-purple p-4"></div>
+    </div>
+  );
+};
--- src/components/partners/PartnerSection.tsx
+++ src/components/partners/PartnerSection.tsx
@@ -1,3 +1,24 @@
+import { SectionContainer } from '../shared/SectionContainer';
+import { PartnerList } from './PartnerList';
+import { SinglePartnerItem } from './SinglePartnerItem';
+
+import utbLogo from '../../assets/img/loga/utb-logo.jpg?url';
+
 export const PartnerSection = () => {
-  return <section>PartnerSection</section>;
+  return (
+    <SectionContainer id="partneri">
+      <div className="space-y-12">
+        <h2 className="text-h2-reversed text-orange">Partneři podporující studentský Majáles UTB</h2>
+        <div className="flex flex-wrap justify-between">
+          <SinglePartnerItem headline="Pořadatel" img="" alt="SU logo" />
+          <SinglePartnerItem headline="Spolupořadatel" img={utbLogo} alt="UTB logo" />
+        </div>
+        <div className="flex flex-col items-center">
+          <SinglePartnerItem headline="Mediální partner" img="" alt="Hitradio logo" />
+          <SinglePartnerItem headline="Generální partner" img="" alt="Trinity logo" />
+        </div>
+        <PartnerList />
+      </div>
+    </SectionContainer>
+  );
 };
