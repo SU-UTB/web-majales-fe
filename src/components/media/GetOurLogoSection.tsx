e18e3dcfc5443925d42f01aba1a85f558629a5f4
Filip Tomes <filiptomes@protonmail.com>
2024-03-03 22:57:18 +0100

    style: media section

---
+++ src/components/media/GetOurLogoSection.tsx
@@ -0,0 +1,22 @@
+import { SectionContainer } from '../shared/SectionContainer';
+
+export const GetOurLogoSection = () => {
+  return (
+    <SectionContainer id="media-logo">
+      <div className="max-w-2xl space-y-8">
+        <p>
+          Sháníte naše logo, tiskovou zprávu nebo třeba fotografie z loňského ročníku? Vše jsme pro vás přehledně
+          připravili tady.
+        </p>
+        <a
+          href="https://drive.google.com/drive/folders/1yJkrVtKaPwjG8hMc4S5ObNAtqRgY9PTI"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bg-blue px-6 py-2 rounded-xl text-beige inline-block"
+        >
+          Stáhnout
+        </a>
+      </div>
+    </SectionContainer>
+  );
+};
