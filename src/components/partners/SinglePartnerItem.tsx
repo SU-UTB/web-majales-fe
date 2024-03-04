5190cc2c0e4a83164aa22e96a97b263d9d37c04f
Filip Tomes <filiptomes@protonmail.com>
2024-03-04 10:29:33 +0100

    style: main partners

--- src/components/partners/SinglePartnerItem.tsx
+++ src/components/partners/SinglePartnerItem.tsx
@@ -1,5 +1,5 @@
 type Props = {
-  headline: string;
+  headline?: string;
   img: string;
   alt: string;
 };
@@ -7,7 +7,7 @@ type Props = {
 export const SinglePartnerItem = ({ headline, img, alt }: Props) => {
   return (
     <div className="space-y-4">
-      <h4 className="text-blue text-h2 text-center">{headline}</h4>
+      {headline && <h4 className="text-blue text-h2 text-center">{headline}</h4>}
       <img src={img} alt={alt} width={400} height={300} className="h-[90px] object-contain" />
     </div>
   );
