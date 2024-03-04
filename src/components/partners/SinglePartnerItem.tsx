0951563bb67c9aa4f8d515b2d420b219ea381439
Filip Tomes <filiptomes@protonmail.com>
2024-03-04 10:20:01 +0100

    style: updated logo resolutions

--- src/components/partners/SinglePartnerItem.tsx
+++ src/components/partners/SinglePartnerItem.tsx
@@ -6,9 +6,9 @@ type Props = {
 
 export const SinglePartnerItem = ({ headline, img, alt }: Props) => {
   return (
-    <div className="space-y-8">
+    <div className="space-y-4">
       <h4 className="text-blue text-h2 text-center">{headline}</h4>
-      <img src={img} alt={alt} width={400} height={300} />
+      <img src={img} alt={alt} width={400} height={300} className="h-[90px] object-contain" />
     </div>
   );
 };
