c87f31a17188652046528d3a58807743b6368e6c
Filip Tomes <filiptomes@protonmail.com>
2024-03-07 10:02:25 +0100

    html semantic update

--- src/components/ubc/AboutSection.tsx
+++ src/components/ubc/AboutSection.tsx
@@ -3,13 +3,13 @@ import { SectionContainer } from '../shared/SectionContainer';
 export const AboutSection = () => {
   return (
     <SectionContainer id="ubc" bgColor="orange">
-      <h2 className="text-beige text-h2">UBC – UniBandContest 2024</h2>
+      <h1 className="text-beige text-h2">UBC – UniBandContest 2024</h1>
       <div className="text-beige space-y-4">
         <p>
           Máte kapelu a chcete si zahrát na velkých pódiích? UniBand Contest je soutěž právě pro vás! Zapoj se do tohoto
           celorepublikového souboje. Získej titul vítěze a status nejlepší studentské kapely v Česku.
         </p>
-        <article>
+        <div>
           <h3 className="text-h2">Informace o UniBand Contestu</h3>
           <p>
             Hlavním cílem soutěže je nabídnout studentským kapelám prostor ukázat svůj talent široké veřejnosti.
@@ -17,7 +17,7 @@ export const AboutSection = () => {
             uměleckém růstu. Kapely se utkají v několika kolech, kde změří své dovednosti a ti nejúspěšnější se dostanou
             „z malého klubu, až na velký festival.“
           </p>
-        </article>
+        </div>
       </div>
     </SectionContainer>
   );
