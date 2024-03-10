603a597252e0057499a8df27cf342f7da23a6a4a
Filip Tomes <filiptomes@protonmail.com>
2024-03-10 21:03:58 +0100

    style: landing section paddings update, bubble - position and width update

--- src/components/home/LandingSection.tsx
+++ src/components/home/LandingSection.tsx
@@ -7,13 +7,13 @@ import { MajalesHeadline } from '../svg/MajalesHeadline';
 
 export const LandingSection = () => {
   return (
-    <section id="landing" className="py-12 xl:py-20">
+    <section id="landing" className="pt-12 pb-32 xl:py-20">
       <Container>
         <div className="relative flex justify-center">
           <MajalesHeadline width={706} />
-          <div className="absolute z-10 top-[80%] left-[10%]">
+          <div className="absolute z-10 top-full md:top-[80%] left-0 md:left-[10%]">
             <HeadlineBubble fillColor="#4400FF" rotationValue={10}>
-              <h2 className="text-h2 text-beige">{MAJALES_DATE.format('D. M. YYYY')}</h2>
+              <h2 className="text-h2-mobile md:text-h2 text-beige">{MAJALES_DATE.format('D. M. YYYY')}</h2>
             </HeadlineBubble>
           </div>
         </div>
