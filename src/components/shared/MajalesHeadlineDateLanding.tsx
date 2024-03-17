73abcd23361e8d7550d01390821ec34ba0af7371
Filip Tomes <filiptomes@protonmail.com>
2024-03-17 16:41:05 +0100

    style: add majales heading backgrounds

--- src/components/shared/MajalesHeadlineDateLanding.tsx
+++ src/components/shared/MajalesHeadlineDateLanding.tsx
@@ -1,14 +1,20 @@
 import { MajalesHeadline } from '../svg/MajalesHeadline';
 import { MajalesHeadlineDate } from '../svg/MajalesHeadlineDate';
-import { Container } from './Container';
+import { SectionContainer } from './SectionContainer';
 
 export const MajalesHeadlineDateLanding = () => {
   return (
-    <Container>
+    <SectionContainer
+      id="majales-headline"
+      style={{
+        backgroundImage: "url('/assets/img/majales-heading-left.webp'), url('/assets/img/majales-heading-right.webp')",
+        backgroundPosition: 'left, right',
+      }}
+    >
       <div className="flex flex-wrap justify-center items-center gap-8 lg:gap-16 py-16">
         <MajalesHeadline width={410} />
         <MajalesHeadlineDate />
       </div>
-    </Container>
+    </SectionContainer>
   );
 };
