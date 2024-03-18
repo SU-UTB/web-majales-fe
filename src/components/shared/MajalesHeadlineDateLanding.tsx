61e153499d4987373de6cb896fc4a1cb4f425aaa
Filip Tomes <filiptomes@protonmail.com>
2024-03-18 12:24:17 +0100

    style: qna backgrounds

--- src/components/shared/MajalesHeadlineDateLanding.tsx
+++ src/components/shared/MajalesHeadlineDateLanding.tsx
@@ -1,14 +1,23 @@
+import { MAJALES_HEADLINE_DATE_LANDING_LEFT_BG, MAJALES_HEADLINE_DATE_LANDING_RIGHT_BG } from '../../config/constants';
 import { MajalesHeadline } from '../svg/MajalesHeadline';
 import { MajalesHeadlineDate } from '../svg/MajalesHeadlineDate';
 import { SectionContainer } from './SectionContainer';
 
-export const MajalesHeadlineDateLanding = () => {
+type Props = {
+  leftBg?: string;
+  rightBg?: string | null;
+};
+
+export const MajalesHeadlineDateLanding = ({
+  leftBg = MAJALES_HEADLINE_DATE_LANDING_LEFT_BG,
+  rightBg = MAJALES_HEADLINE_DATE_LANDING_RIGHT_BG,
+}: Props) => {
   return (
     <SectionContainer
       id="majales-headline"
       style={{
-        backgroundImage: "url('/assets/img/majales-heading-left.webp'), url('/assets/img/majales-heading-right.webp')",
-        backgroundPosition: 'left, right',
+        backgroundImage: leftBg && rightBg ? `url(${leftBg}), url(${rightBg})` : `url(${leftBg})`,
+        backgroundPosition: leftBg && rightBg ? 'left, right' : 'left',
       }}
     >
       <div className="flex flex-wrap justify-center items-center gap-8 lg:gap-16 py-16">
--- src/config/constants.ts
+++ src/config/constants.ts
@@ -3,3 +3,6 @@ import dayjs from 'dayjs';
 export const MAJALES_DATE = dayjs('2024-05-03');
 export const COPYRIGHT_TEXT = `©Studentská unie utb - ${MAJALES_DATE.year()}`;
 export const MAJALES_TITLE = 'Majáles UTB Zlín';
+
+export const MAJALES_HEADLINE_DATE_LANDING_LEFT_BG = '/assets/img/majales-heading-left.webp';
+export const MAJALES_HEADLINE_DATE_LANDING_RIGHT_BG = '/assets/img/majales-heading-right.webp';
