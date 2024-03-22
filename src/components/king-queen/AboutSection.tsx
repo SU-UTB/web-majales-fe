a6bd885c6e87dd0a5306b48101a184767fc2bf06
Filip Tomes <60915718+tomesfilip@users.noreply.github.com>
2024-03-22 16:17:07 +0100

    Fix/review 1.0 king queen (#38)
    * form link
    
    * date fixes, add links

--- src/components/king-queen/AboutSection.tsx
+++ src/components/king-queen/AboutSection.tsx
@@ -7,7 +7,7 @@ export const AboutSection = () => {
     <SectionContainer id="kral-a-kralovna" bgColor="purple">
       <div className="space-y-12">
         <AboutContent />
-        <Register />
+        <Register registerLink="https://docs.google.com/forms/d/113IkYHtUnrcFXTQybnU9GNiz4d0crpIdnoFt3w1alew/viewform" />
       </div>
     </SectionContainer>
   );
--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,6 +1,7 @@
 import dayjs from 'dayjs';
 
 export const MAJALES_DATE = dayjs('2024-05-03');
+export const KING_QUEEN_REGISTRATION_DATE = dayjs('2024-04-01');
 export const COPYRIGHT_TEXT = `©Studentská unie utb - ${MAJALES_DATE.year()}`;
 export const MAJALES_TITLE = 'Majáles UTB Zlín';
 
