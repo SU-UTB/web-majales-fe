49404cf30fb4fb10821850884f11c0eb1c665cde
Filip Tomes <filiptomes@protonmail.com>
2024-03-22 12:45:47 +0100

    form link

--- src/components/king-queen/Register.tsx
+++ src/components/king-queen/Register.tsx
@@ -1,9 +1,13 @@
+import { ExternalLink } from '../shared/ExternalLink';
+
 export const Register = () => {
   return (
     <div className="flex flex-wrap gap-4 md:gap-8 items-center">
-      <a href="/" className="inline-block px-6 py-2 text-black bg-beige rounded-xl">
-        Registrovat
-      </a>
+      <ExternalLink
+        link="https://docs.google.com/forms/d/113IkYHtUnrcFXTQybnU9GNiz4d0crpIdnoFt3w1alew/viewform"
+        text="Registrovat"
+        className="inline-block px-6 py-2 text-black bg-beige rounded-xl"
+      />
       <p className="-skew-x-3 bg-beige px-2">registrace se spouští od 1. 4. 2024</p>
     </div>
   );
