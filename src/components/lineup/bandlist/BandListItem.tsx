a2fe33bbbbb184bc64fe341dd8858d4f35fff61e
Filip Tomes <filiptomes@protonmail.com>
2024-04-21 19:51:59 +0200

    style: content update

--- src/components/lineup/bandlist/BandListItem.tsx
+++ src/components/lineup/bandlist/BandListItem.tsx
@@ -1,13 +1,8 @@
 import type { BandType } from '../../../AppTypes';
 
-export const BandListItem = ({ img, title, link }: BandType) => {
+export const BandListItem = ({ img, title }: BandType) => {
   return (
-    <a
-      className="relative group overflow-hidden w-full max-w-[432px] h-[238px] rounded-xl"
-      target="_blank"
-      rel="noopener noreferrer external"
-      href={link}
-    >
+    <div className="relative group overflow-hidden w-full max-w-[432px] h-[238px] rounded-xl">
       <p className="text-beige text-p-mobile md:text-useful-link z-20 absolute left-4 bottom-4">{title}</p>
       <div className="inline-block absolute bottom-0 w-0 h-0 border-solid border-t-[100px] sm:border-t-[140px] border-r-0 border-l-[460px] border-b-0 border-l-blue border-r-transparent border-t-transparent border-b-transparent z-10"></div>
       <img
@@ -15,6 +10,6 @@ export const BandListItem = ({ img, title, link }: BandType) => {
         alt={title}
         className="w-full h-full object-cover object-center group-hover:scale-105 transition-transform z-0"
       />
-    </a>
+    </div>
   );
 };
--- src/data/bands.ts
+++ src/data/bands.ts
@@ -1,7 +1,6 @@
 import type { BandType } from '../AppTypes';
 
 import annetXAndBobodyListen from '../assets/img/bands/annetx-nobody.webp?url';
-import fallAndFell from '../assets/img/bands/fall-fell.webp?url';
 import hospodskyBordel from '../assets/img/bands/hospodsky-bordel.webp?url';
 import nsf from '../assets/img/bands/nsf.webp?url';
 import qocna from '../assets/img/bands/quocna.webp?url';
@@ -14,46 +13,37 @@ export const bands: BandType[] = [
   {
     img: annetXAndBobodyListen,
     title: 'Annet X & Nobody Listen',
-    link: '',
   },
-  {
-    img: fallAndFell,
-    title: 'Fall & Fell',
-    link: '',
-  },
+  // {
+  //   img: fallAndFell,
+  //   title: 'Fall & Fell',
+  // },
   {
     img: hospodskyBordel,
     title: 'Hospodsky Bordel',
-    link: '',
   },
   {
     img: nsf,
     title: 'N.S.F.W.',
-    link: '',
   },
   {
     img: qocna,
     title: 'Qočna',
-    link: '',
   },
   {
     img: rez,
     title: 'REZ',
-    link: '',
   },
   {
     img: zociVoci,
     title: 'Zoči Voči',
-    link: '',
   },
   {
     img: restWich,
-    title: 'Rest a Wich',
-    link: '',
+    title: 'Rest',
   },
   {
     img: restWich1,
-    title: 'Rest a Wich',
-    link: '',
+    title: 'DJ Wich',
   },
 ];
