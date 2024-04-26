78cdad3fe450df7d71c5383c30aa4d11fab86919
Filip Tomes <filiptomes@protonmail.com>
2024-04-26 16:49:58 +0200

    style: content update

--- src/components/lineup/AboutSection.tsx
+++ src/components/lineup/AboutSection.tsx
@@ -1,3 +1,4 @@
+import { CAN_SHOW_BANDS } from '../../config/constants';
 import { ExternalLink } from '../shared/ExternalLink';
 import { SectionContainer } from '../shared/SectionContainer';
 
@@ -6,20 +7,33 @@ export const AboutSection = () => {
     <SectionContainer id="lineup" bgColor="purple">
       <h2 className="text-h2">Line up</h2>
       <div className="max-w-[800px] space-y-4">
-        <p>
-          Kompletní line-up vám zatím neprozradíme, ale těšit se můžete celkem na 8 interpretů. Ti se postupně
-          vystřídají na dvou stagích. A&nbsp;můžeme vám slíbit, že tobude fakt něco. Těšit se totiž můžete na interprety
-          různých žánrů, a&nbsp;to od rocku přes pop až po rap.
-        </p>
-        <p>
-          Už teď vám ale můžeme prozradit, že na Majálesu UTB vystoupí výherci UniBand Contestu. Více zjistíte na&nbsp;
-          <ExternalLink link="https://unibandcontest.cz/" text="unibandcontest.cz" className="underline" />
-        </p>
-        <p>
-          Mimo jiné se můžete zapojit i&nbsp;do doprovodných programů, ve kterých můžete vyhrát hodnotné ceny nebo se
-          prostě jen pořádně zasmát, navštívit Chill-out zónu, kde si odpočinete, nebo si zajít na dobré jídlo
-          a&nbsp;pití. V&nbsp;areálu se rozhodně nudit nebudete.
-        </p>
+        {CAN_SHOW_BANDS ? (
+          <>
+            <p>Zajímá vás, kdo vystoupí na letošním Majálesu UTB 2024?</p>
+            <p>
+              Na dvou stagích zazáří interprety různých žánrů, a to od rocku přes pop až po rap.
+              <br />A proto se letos můžete těšit na:
+            </p>
+          </>
+        ) : (
+          <>
+            <p>
+              Kompletní line-up vám zatím neprozradíme, ale těšit se můžete celkem na 8 interpretů. Ti se postupně
+              vystřídají na dvou stagích. A&nbsp;můžeme vám slíbit, že tobude fakt něco. Těšit se totiž můžete na
+              interprety různých žánrů, a&nbsp;to od rocku přes pop až po rap.
+            </p>
+            <p>
+              Už teď vám ale můžeme prozradit, že na Majálesu UTB vystoupí výherci UniBand Contestu. Více zjistíte
+              na&nbsp;
+              <ExternalLink link="https://unibandcontest.cz/" text="unibandcontest.cz" className="underline" />
+            </p>
+            <p>
+              Mimo jiné se můžete zapojit i&nbsp;do doprovodných programů, ve kterých můžete vyhrát hodnotné ceny nebo
+              se prostě jen pořádně zasmát, navštívit Chill-out zónu, kde si odpočinete, nebo si zajít na dobré jídlo
+              a&nbsp;pití. V&nbsp;areálu se rozhodně nudit nebudete.
+            </p>
+          </>
+        )}
       </div>
     </SectionContainer>
   );
