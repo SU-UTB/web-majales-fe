997d1129f9b5999b3e13d9a44621f107202c92af
DejviFiala <david.fiala.44@gmail.com>
2024-10-29 11:53:14 +0100

    Renamed conatct + added contact icon photos

--- src/components/contacts/ContactItem.tsx
+++ src/components/contacts/ContactItem.tsx
@@ -4,13 +4,24 @@ type Props = ContactType & {
   textColor?: Colors;
 };
 
-export const ContactItem = ({ position, name, email, phone, textColor = 'orange' }: Props) => {
+export const ContactItem = ({ position, name, email, phone, photo, textColor = 'orange' }: Props) => {
   return (
     <div className="flex flex-col">
       <h3 className={`text-${textColor} font-semibold`}>{position}</h3>
-      <p className="uppercase font-medium">{name}</p>
-      <a href={`mailto:${email}`}>{email}</a>
-      <a href={`tel:${phone}`}>{phone}</a>
+      <div className="flex items-center gap-4">
+        <div>
+          <img
+            src={photo}
+            alt="contactIcon"
+            className="rounded-full h-24 w-24 object-cover transform transition-transform duration-200 lg:hover:scale-175 lg:hover:shadow-lg"
+          />
+        </div>
+        <div className="flex flex-col">
+          <p className="uppercase font-medium">{name}</p>
+          <a href={`mailto:${email}`}>{email}</a>
+          <a href={`tel:${phone}`}>{phone}</a>
+        </div>
+      </div>
     </div>
   );
 };
--- src/data/contacts.ts
+++ src/data/contacts.ts
@@ -1,4 +1,12 @@
 import type { ContactType } from '../AppTypes';
+import BetyPhoto from '../assets/img/contacts/bety.jpg?url';
+import DefaultPhoto from '../assets/img/contacts/default.jpg?url';
+import ErikPhoto from '../assets/img/contacts/erik.jpg?url';
+import JstPhoto from '../assets/img/contacts/jst.jpg?url';
+import KlarkaPhoto from '../assets/img/contacts/klarka.jpg?url';
+import LukyPhoto from '../assets/img/contacts/luky.jpg?url';
+import MartaPhoto from '../assets/img/contacts/marta.jpg?url';
+import PataPhoto from '../assets/img/contacts/pata.jpg?url';
 
 export const contacts: ContactType[] = [
   {
@@ -6,47 +14,55 @@ export const contacts: ContactType[] = [
     name: 'PATRIK HÁBL',
     email: 'habl@sutb.cz',
     phone: '+420 777 995 476',
+    photo: PataPhoto,
   },
   {
     position: 'Manažer projektu',
     name: 'LUKÁŠ FAKSA',
     email: 'faksa@sutb.cz',
     phone: '+420 605 456 216',
+    photo: LukyPhoto,
   },
   {
     position: 'Marketing',
     name: 'ALŽBĚTA HRUŠKOVÁ',
     email: 'hruskova@sutb.cz',
     phone: '+420 775 673 216',
+    photo: BetyPhoto,
   },
   {
     position: 'Sales',
     name: 'JANKA NIEPELOVÁ',
     email: 'niepelova@sutb.cz',
     phone: '+421 918 424 947',
+    photo: DefaultPhoto,
   },
   {
     position: 'Media relations',
     name: 'MARTINA MATULÍKOVÁ',
     email: 'matulikova@sutb.cz',
     phone: '+420 730 930 139',
+    photo: MartaPhoto,
   },
   {
     position: 'Produkce',
     name: 'JAKUB STANĚK',
     email: 'stanek@sutb.cz',
     phone: '+420 776 459 859',
+    photo: JstPhoto,
   },
   {
     position: 'Art director',
     name: 'ERIK RIEČIČIAR',
     email: 'rieciciar@sutb.cz',
     phone: '+420 773 834 430',
+    photo: ErikPhoto,
   },
   {
     position: 'Kapely',
-    name: 'DAVID ŠTÍCHA',
-    email: 'sticha@sutb.cz',
-    phone: '+420 775 520 010',
+    name: 'KLÁRA HRDLIČKOVÁ',
+    email: 'hrdlickova@sutb.cz',
+    phone: '+420 604 708 198',
+    photo: KlarkaPhoto,
   },
 ];
