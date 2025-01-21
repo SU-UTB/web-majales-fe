72cc1641bb09e16103b2d87139530a15178d68f0
Pavos21 <metalmasterboy@gmail.com>
2025-01-21 16:19:49 +0100

    Section Hero creation

---
+++ src/components/shared/SectionHero.tsx
@@ -0,0 +1,44 @@
+import { MAJALES_DATE } from "../../config/constants";
+import { CheckerRectangle } from "../svg/elements/CheckerRectangle";
+import { CheckerSquare } from "../svg/elements/CheckerSquare";
+import { AsteriskSquare } from "../svg/elements/hero/AsteriskSquare";
+import { DateRectangle } from "../svg/elements/hero/DateRectangle";
+import { Triangle } from "../svg/elements/hero/Triangle";
+import { MajalesHeadline } from "../svg/MajalesHeadline";
+import { SectionContainer } from "./SectionContainer";
+
+export const SectionHero = ({ dateRectColor, BLT1, BLT2, BRT1, BRS1, BRT2, BRT3 } : { dateRectColor: string, BLT1: string, BLT2: string, BRT1: string, BRS1: string, BRT2: string, BRT3: string }) => {
+  return(
+    <SectionContainer
+      id="majales-headline"
+      className="pt-10 pb-24 md:pt-20 md:pb-32 xl:py-24"
+    >
+      <div className="relative flex justify-center">
+        <MajalesHeadline width={500} />
+      </div>
+      {/* BOTTOM LEFT */}
+      <div className="absolute bottom-0 left-0 z-10">
+        <div className="relative">
+          <Triangle className="absolute bottom-24 md:bottom-48 left-0 md:left-0 h-12 md:h-24 -rotate-90" color={BLT1} />
+          <CheckerSquare className="absolute bottom-12 md:bottom-24 left-0 md:left-0 h-12 md:h-24" />
+          <DateRectangle className="absolute bottom-0 md:bottom-0 left-0 md:left-0 h-12 md:h-24 w-36 md:w-72 overflow-hidden" color={dateRectColor}>
+            <h2 className="text-h2-reversed-mobile md:text-h2-reversed text-white">
+              {MAJALES_DATE.toLocaleDateString('cs-CZ')}
+            </h2>
+          </DateRectangle>
+          <Triangle className="absolute bottom-0 md:bottom-0 left-36 md:left-72 h-12 md:h-24 -rotate-90" color={BLT2} />
+        </div>
+      </div>
+      {/* BOTTOM RIGHT */}
+      <div className="absolute bottom-0 right-0 z-0">
+        <div className="relative">
+          <Triangle className="absolute -left-12 md:-left-24 h-12 md:h-24 -rotate-180" color={BRT1} />
+          <CheckerRectangle className="h-12 md:h-24" />
+          <AsteriskSquare className="absolute right-0 md:right-0 bottom-12 md:bottom-24 h-12 md:h-24" color={BRS1} /> 
+          <Triangle className="absolute right-12 md:right-24 bottom-12 md:bottom-24 h-12 md:h-24 -rotate-180" color={BRT2} />
+          <Triangle className="absolute right-0 md:right-0 bottom-24 md:bottom-48 h-12 md:h-24 -rotate-180" color={BRT3} />
+        </div>
+      </div>
+    </SectionContainer>
+  );
+};
---
+++ src/components/svg/elements/hero/Triangle.tsx
@@ -0,0 +1,12 @@
+export const Triangle = ({ className, color }: { className?: string, color: string }) => {
+  return (
+    <svg
+      className={`w-auto ${className}`}
+      viewBox="0 0 131 131"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M-5.72619e-06 131L131 0L0 -5.72619e-06L-5.72619e-06 131Z" fill={color}/>
+    </svg>
+  );
+};
