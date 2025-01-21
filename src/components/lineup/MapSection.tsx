76f6062a02e4987b1faa76b15f6c17f179406ebc
Pavos21 <metalmasterboy@gmail.com>
2025-01-21 20:16:39 +0100

    Map style update

--- src/components/lineup/MapSection.tsx
+++ src/components/lineup/MapSection.tsx
@@ -1,13 +1,36 @@
 import { SectionContainer } from '../shared/SectionContainer';
+import { CheckerSquare } from '../svg/elements/CheckerSquare';
+import { Triangle } from '../svg/elements/hero/Triangle';
+import { TextRectangle } from '../svg/elements/TextRectangle';
 
 import { MajalesAreaMap } from '../svg/MajalesAreaMap';
 
 export const MapSection = () => {
   return (
     <SectionContainer id="mapa" bgColor="lightpink">
-      <div className="space-y-8">
-        <h2 className="text-h2-reversed text-beige">Mapa areálu</h2>
-        <MajalesAreaMap width={900} />
+      <div className="pt-12 md:pt-20 px-12 md:px-16 flex justify-center items-center h-full">
+        <MajalesAreaMap width={600} />
+      </div>
+
+      {/* TOP LEFT */}
+      <div className="absolute top-0 left-0">
+        <div className="relative">
+          <Triangle className="absolute top-24 md:top-48 left-0 md:left-0 h-12 md:h-24" color='#E7548A' />
+          <CheckerSquare className="absolute top-12 md:top-24 left-0 md:left-0 h-12 md:h-24" />
+          <TextRectangle className="absolute top-0 md:top-0 left-0 md:left-0 h-12 md:h-24 w-48 md:w-96 overflow-y-hidden" color='#6459FD'>
+            <h2 className="text-h2-reversed-mobile md:text-h2-reversed text-white">
+              Mapa areálu
+            </h2>
+          </TextRectangle>           
+          <Triangle className="absolute top-0 md:top-0 left-48 md:left-96 h-12 md:h-24" color='#FFFFFF' />
+        </div>
+      </div>
+
+      {/* BOTTOM RIGHT */}
+      <div className="absolute bottom-0 right-0">
+        <div className="relative">
+          <Triangle className="absolute -top-12 md:-top-24 right-0 md:right-0 h-12 md:h-24 rotate-180" color='#1F2041' />
+        </div>
       </div>
     </SectionContainer>
   );
