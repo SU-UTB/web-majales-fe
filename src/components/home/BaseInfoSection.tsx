5a560aa9fefac48123040dd0db0e699debe4b627
Pavos21 <metalmasterboy@gmail.com>
2025-01-26 20:05:23 +0100

    Text alignment fix

--- src/components/home/BaseInfoSection.tsx
+++ src/components/home/BaseInfoSection.tsx
@@ -2,17 +2,21 @@ import { SectionContainer } from '../shared/SectionContainer';
 
 export const BaseInfoSection = () => {
   return (
-    <SectionContainer id="zakladni-informae" bgColor="lightpink">
+    <SectionContainer id="zakladni-informace" bgColor="lightpink">
       <div className="flex flex-wrap gap-8 xl:gap-12 w-full lg:justify-between py-8 xl:py-16 xl:text-p-big items-center justify-center">
-        <div className="w-full max-w-[685px] lg:max-w-max space-y-2 text-p-big text-white">
-          <p>
-            Vstup <span className="font-semibold">ZDARMA</span> pro všechny
-          </p>
-          <p>2 stage s 8 interprety </p>
-          <p>Food & Chill-out zóna</p>
-          <p>Doprovodný program</p>
-          <p>Král a Královna Majálesu</p>
-          <p className="text-h2-reversed-mobile md:text-h3-reversed">To a mnohem víc na Majálesu UTB!</p>
+        <div className="w-full max-w-[685px] lg:max-w-max flex flex-col justify-between h-full text-p-big text-white">
+          <div className="space-y-2">
+            <p>
+              Vstup <span className="font-semibold">ZDARMA</span> pro všechny
+            </p>
+            <p>2 stage s 8 interprety </p>
+            <p>Food & Chill-out zóna</p>
+            <p>Doprovodný program</p>
+          </div>
+          <div className="mt-auto">
+            <p>Král a Královna Majálesu</p>
+            <p className="text-h2-reversed-mobile md:text-h3-reversed">To a mnohem víc na Majálesu UTB!</p>
+          </div>
         </div>
         <iframe
           width="685"
