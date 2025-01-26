200f7cb19cdd11f91836230d40ee4c44a90185d3
Pavos21 <metalmasterboy@gmail.com>
2025-01-26 19:57:15 +0100

    Kontakty div above footer

---
+++ src/components/shared/UpperFooter.tsx
@@ -0,0 +1,7 @@
+export const UpperFooter: React.FC = () => {
+  return (
+    <div className="w-full h-20 flex items-center justify-center bg-blue">
+      <h1 className="text-h2-reversed-mobile md:text-h2-reversed text-white">Kontakty</h1>
+    </div>
+  );
+};
