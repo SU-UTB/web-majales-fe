72d467d4a0981c548ce37829ccc186a3f483a0d4
DejviFiala <david.fiala.44@gmail.com>
2025-03-04 17:43:26 +0100

    Edited new aspec ratio for band images

--- src/components/lineup/BandListSection.tsx
+++ src/components/lineup/BandListSection.tsx
@@ -3,7 +3,6 @@ import { bands } from '../../data/bands';
 import { SectionContainer } from '../shared/SectionContainer';
 import { ArrowSquare } from '../svg/elements/ArrowSquare';
 import { BigTriangle } from '../svg/elements/BigTriangle';
-import { CheckerRectanglePortrait } from '../svg/elements/CheckerRectanglePortrait';
 import { CheckerSquare } from '../svg/elements/CheckerSquare';
 import { Triangle } from '../svg/elements/Triangle';
 import { BandListItem } from './bandlist/BandListItem';
@@ -24,17 +23,31 @@ export const BandListSection = () => {
       {/* TOP RIGHT */}
       <div className="absolute top-0 right-0">
         <div className="relative">
-          <Triangle className="absolute -top-12 md:-top-24 right-0 md:right-0 h-12 md:h-24 rotate-180" color='#FFFFFF' />
-          <CheckerSquare className="absolute top-0 md:top-0 right-0 md:right-0 h-12 md:h-24" color1='#FFE2DE' color2='#E7548A' />
-          <CheckerSquare className="absolute top-12 md:top-24 right-0 md:right-0 h-12 md:h-24" color1='#E7548A' color2='#FFE2DE' />
-          <ArrowSquare className="absolute top-24 md:top-48 right-0 md:right-0 h-12 md:h-24" color='#FFFFFF'/>
-          <Triangle className="absolute top-36 md:top-72 right-0 md:right-0 h-12 md:h-24 rotate-90" color='#FF785A' />
+          <Triangle
+            className="absolute -top-12 md:-top-24 right-0 md:right-0 h-12 md:h-24 rotate-180"
+            color="#FFFFFF"
+          />
+          <CheckerSquare
+            className="absolute top-0 md:top-0 right-0 md:right-0 h-12 md:h-24"
+            color1="#FFE2DE"
+            color2="#E7548A"
+          />
+          <CheckerSquare
+            className="absolute top-12 md:top-24 right-0 md:right-0 h-12 md:h-24"
+            color1="#E7548A"
+            color2="#FFE2DE"
+          />
+          <ArrowSquare className="absolute top-24 md:top-48 right-0 md:right-0 h-12 md:h-24" color="#FFFFFF" />
+          <Triangle className="absolute top-36 md:top-72 right-0 md:right-0 h-12 md:h-24 rotate-90" color="#FF785A" />
         </div>
       </div>
       {/* BOTTOM LEFT */}
       <div className="absolute bottom-0 left-0">
         <div className="relative">
-          <BigTriangle className="absolute -top-24 md:-top-48 left-0 md:left-0 h-24 md:h-48 rotate-180" color='#FFFFFF'/>
+          <BigTriangle
+            className="absolute -top-24 md:-top-48 left-0 md:left-0 h-24 md:h-48 rotate-180"
+            color="#FFFFFF"
+          />
         </div>
       </div>
     </SectionContainer>
--- src/data/bands.ts
+++ src/data/bands.ts
@@ -1,49 +1,50 @@
 import type { BandType } from '../AppTypes';
 
-import annetXAndBobodyListen from '../assets/img/bands/annetx-nobody.webp?url';
-import hospodskyBordel from '../assets/img/bands/hospodsky-bordel.webp?url';
-import nsf from '../assets/img/bands/nsf.webp?url';
-import qocna from '../assets/img/bands/quocna.webp?url';
-import restWich1 from '../assets/img/bands/rest-wich-1.webp?url';
-import restWich from '../assets/img/bands/rest-wich.webp?url';
-import rez from '../assets/img/bands/rez.webp?url';
-import zociVoci from '../assets/img/bands/zoci-voci.webp?url';
-
+import qocna_post from '../assets/img/bands/qocna_post.webp?url';
+import saraNoff from '../assets/img/bands/sara-noff.webp?url';
 export const bands: BandType[] = [
   {
-    img: annetXAndBobodyListen,
-    title: 'Annet X & Nobody Listen',
-  },
-  // {
-  //   img: fallAndFell,
-  //   title: 'Fall & Fell',
-  // },
-  {
-    img: hospodskyBordel,
-    title: 'Hospodsky Bordel',
+    img: saraNoff,
+    title: 'Sara Noff',
   },
   {
-    img: nsf,
-    title: 'N.S.F.W.',
-  },
-  {
-    img: qocna,
+    img: qocna_post,
     title: 'Qočna',
   },
   {
-    img: rez,
-    title: 'REZ',
+    img: qocna_post,
+    title: 'Qočna',
   },
   {
-    img: zociVoci,
-    title: 'Zoči Voči',
+    img: saraNoff,
+    title: 'Sara Noff',
   },
-  {
-    img: restWich,
-    title: 'Rest',
-  },
-  {
-    img: restWich1,
-    title: 'DJ Wich',
-  },
+  // {
+  //   img: hospodskyBordel,
+  //   title: 'Hospodsky Bordel',
+  // },
+  // {
+  //   img: nsf,
+  //   title: 'N.S.F.W.',
+  // },
+  // {
+  //   img: qocna,
+  //   title: 'Qočna',
+  // },
+  // {
+  //   img: rez,
+  //   title: 'REZ',
+  // },
+  // {
+  //   img: zociVoci,
+  //   title: 'Zoči Voči',
+  // },
+  // {
+  //   img: restWich,
+  //   title: 'Rest',
+  // },
+  // {
+  //   img: restWich1,
+  //   title: 'DJ Wich',
+  // },
 ];
