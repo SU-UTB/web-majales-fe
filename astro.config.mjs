4282e38f6d4c54b010bab9513328704601a7e578
metalmasterboy <metalmasterboy@gmail.com>
2025-04-11 11:44:43 +0000

    Cookies consent

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import robotsTxt from 'astro-robots-txt';
 import { defineConfig } from 'astro/config';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://majales.utb.cz/',
+  server: {
+    host: true, // Allow local network access
+  },
   integrations: [tailwind(), react(), robotsTxt(), sitemap()],
 });
