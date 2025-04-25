a4ca5c0c1964611d6fbd5aa3fc20aaaef900a5fd
metalmasterboy <metalmasterboy@gmail.com>
2025-04-25 20:53:01 +0000

    Added starting time to header

--- src/components/shared/SectionHero.tsx
+++ src/components/shared/SectionHero.tsx
@@ -1,4 +1,5 @@
 import { MAJALES_DATE } from "../../config/constants";
+import { MAJALES_TIME } from "../../config/constants";
 import { AsteriskSquare } from "../svg/elements/AsteriskSquare";
 import { CheckerSquare } from "../svg/elements/CheckerSquare";
 import { DateRectangle } from "../svg/elements/DateRectangle";
@@ -11,15 +12,14 @@ type Props = {
   BottomLeftTriangle1: string, 
   BottomLeftTriangle2: string, 
   BottomRightTriangle1: string, 
-  BottomRightCheckerSquare1: string,
-  BottomRightCheckerSquare2: string,
+  timeRectColor: string;
   BottomRightSquare: string, 
   BottomRightSquareAsterisk: string, 
   BottomRightTriangle2: string, 
   BottomRightTriangle3: string
 };
 
-export const SectionHero = ({ dateRectColor, BottomLeftTriangle1, BottomLeftTriangle2, BottomRightTriangle1, BottomRightCheckerSquare1, BottomRightCheckerSquare2, BottomRightSquare, BottomRightSquareAsterisk, BottomRightTriangle2, BottomRightTriangle3 } : Props) => {
+export const SectionHero = ({ dateRectColor, BottomLeftTriangle1, BottomLeftTriangle2, BottomRightTriangle1, timeRectColor, BottomRightSquare, BottomRightSquareAsterisk, BottomRightTriangle2, BottomRightTriangle3 } : Props) => {
   return(
     <SectionContainer
       id="majales-headline"
@@ -32,10 +32,11 @@ export const SectionHero = ({ dateRectColor, BottomLeftTriangle1, BottomLeftTria
       <div className="absolute bottom-0 right-0 z-0">
         <div className="relative">
           <Triangle className="absolute right-36 md:right-72 bottom-0 md:bottom-0 h-12 md:h-24 -rotate-180" color={BottomRightTriangle1} />
-          {/* <CheckerRectangleLandscape className="h-12 md:h-24" color1="#1F2041" color2="#FFE2DE" /> */}
-          <CheckerSquare className="absolute right-24 md:right-48 bottom-0 md:bottom-0 h-12 md:h-24" color1={BottomRightCheckerSquare1} color2={BottomRightCheckerSquare2} />
-          <CheckerSquare className="absolute right-12 md:right-24 bottom-0 md:bottom-0 h-12 md:h-24" color1={BottomRightCheckerSquare2} color2={BottomRightCheckerSquare1} />
-          <CheckerSquare className="absolute right-0 md:right-0 bottom-0 md:bottom-0 h-12 md:h-24" color1={BottomRightCheckerSquare1} color2={BottomRightCheckerSquare2} />
+          <DateRectangle className="absolute bottom-0 md:bottom-0 right-0 md:right-0 h-12 md:h-24 w-36 md:w-72 overflow-hidden" color={timeRectColor}>
+            <h2 className="text-h2-reversed-mobile md:text-h2-reversed text-white">
+              {MAJALES_TIME}
+            </h2>
+          </DateRectangle>
           <AsteriskSquare className="absolute right-0 md:right-0 bottom-12 md:bottom-24 h-12 md:h-24" color={BottomRightSquare} asteriskColor={BottomRightSquareAsterisk} /> 
           <Triangle className="absolute right-12 md:right-24 bottom-12 md:bottom-24 h-12 md:h-24 -rotate-180" color={BottomRightTriangle2} />
           <Triangle className="absolute right-0 md:right-0 bottom-24 md:bottom-48 h-12 md:h-24 -rotate-180" color={BottomRightTriangle3} />
