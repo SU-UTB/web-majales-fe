b3a855df43cf6d9541729638eca09b341ec4a340
Pavos21 <metalmasterboy@gmail.com>
2026-02-18 11:01:13 +0100

    Showup prepare

--- src/components/lineup/BandListSection.tsx
+++ src/components/lineup/BandListSection.tsx
@@ -1,35 +1,38 @@
 import { CAN_SHOW_BANDS } from '../../config/constants';
 import { bands } from '../../data/bands';
 import { SectionContainer } from '../shared/SectionContainer';
-import { ArrowSquare } from '../svg/elements/ArrowSquare';
-import { BigTriangle } from '../svg/elements/BigTriangle';
-import { Triangle } from '../svg/elements/Triangle';
 import { BandListItem } from './bandlist/BandListItem';
 import { BandListItemHeadliner } from './bandlist/BandListItemHeadliner';
 
 export const BandListSection = () => {
+  const now = new Date();
+  const hasHiddenBands = bands.some((band) => now < band.visibleFrom);
+
   return (
     <SectionContainer id="kapely" bgColor="violet" className='text-white'>
       <h2 className="text-3xl p-6 md:text-6xl font-medium text-center relative z-20">Na koho se můžete těšit?</h2>
       {CAN_SHOW_BANDS ? (
-      <div className="relative z-20">
-        {/* Headliners */}
-        <div className="space-y-4 md:space-y-8 md:px-4">
-          {bands.slice(0, 2).map((band) => (
-            <BandListItemHeadliner key={band.title} {...band} />
-          ))}
-        </div>
+        <div className="relative z-20">
+          {/* Headliners */}
+          <div className="space-y-4 md:space-y-8 md:px-4">
+            {bands.slice(0, 2).filter(band => now >= band.visibleFrom).map((band) => (
+              <BandListItemHeadliner key={band.title} {...band} />
+            ))}
+          </div>
 
-        {/* Remaining bands */}
-        <div className="pt-6 md:px-4 grid grid-cols-2 lg:grid-cols-4 gap-4">
-          {bands.slice(2).map((band) => (
-            <BandListItem key={band.title} {...band} />
-          ))}
+          {/* Remaining bands */}
+          <div className="pt-6 md:px-4 grid grid-cols-2 lg:grid-cols-4 gap-4">
+            {bands.slice(2).filter(band => now >= band.visibleFrom).map((band) => (
+              <BandListItem key={band.title} {...band} />
+            ))}
+          </div>
+          {hasHiddenBands && (
+            <p className="text-xl md:text-3xl py-12 font-normal text-center relative z-20">Brzy představíme další...</p>
+          )}
         </div>
-      </div>
-    ) : (
-      <p className="text-xl md:text-3xl py-12 font-normal text-center relative z-20">Již brzy prozradíme</p>
-    )}
+      ) : (
+        <p className="text-xl md:text-3xl py-12 font-normal text-center relative z-20">Již brzy prozradíme</p>
+      )}
     </SectionContainer>
   );
 };
--- src/data/bands.ts
+++ src/data/bands.ts
@@ -1,54 +1,64 @@
 import type { BandType } from '../AppTypes';
 
-import qocna from '../assets/img/bands/qocna.webp?url';
-import saraNoff from '../assets/img/bands/saranoff.webp?url';
-import novaStopa from '../assets/img/bands/novastopa.webp?url';
-import echo from '../assets/img/bands/echo.webp?url';
-import johnny from '../assets/img/bands/johnnycurbes.webp?url';
-import marko from '../assets/img/bands/markodamian.webp?url';
-import focus from '../assets/img/bands/focusrock.webp?url';
 import misik from '../assets/img/bands/adammisik.webp?url';
+import echo from '../assets/img/bands/echo.webp?url';
+import focus from '../assets/img/bands/focusrock.webp?url';
+import johnny from '../assets/img/bands/johnnycurbes.webp?url';
 import kharma from '../assets/img/bands/kharmadj.webp?url';
+import marko from '../assets/img/bands/markodamian.webp?url';
+import novaStopa from '../assets/img/bands/novastopa.webp?url';
+import qocna from '../assets/img/bands/qocna.webp?url';
 import renne from '../assets/img/bands/rennedang.webp?url';
-export const bands: BandType[] = [
+import saraNoff from '../assets/img/bands/saranoff.webp?url';
+export const bands: (BandType & { visibleFrom: Date })[] = [
   {
     img: renne,
     title: 'Renne Dang',
+    visibleFrom: new Date('2026-04-02T18:00:00'),
   },
   {
     img: misik,
     title: 'Adam Mišík',
+    visibleFrom: new Date('2026-03-30T18:00:00'),
   },
   {
     img: marko,
     title: 'Marko Damian',
+    visibleFrom: new Date('2026-03-24T18:00:00'),
   },
   {
     img: focus,
     title: 'Focus Rock',
+    visibleFrom: new Date('2026-04-14T18:00:00'),
   },
   {
     img: saraNoff,
     title: 'Sara Noff',
+    visibleFrom: new Date('2026-03-16T18:00:00'),
   },
   {
     img: novaStopa,
     title: 'Nová stopa',
+    visibleFrom: new Date('2026-03-10T18:00:00'),
   },
   {
     img: kharma,
     title: 'Kharma DJ',
+    visibleFrom: new Date('2026-03-06T18:00:00'),
   },
   {
     img: johnny,
     title: 'Johnny Čurbes',
+    visibleFrom: new Date('2026-03-02T18:00:00'),
   },
   {
     img: echo,
     title: 'echo',
+    visibleFrom: new Date('2026-02-10T18:00:00'),
   },
   {
     img: qocna,
     title: 'Qočna',
+    visibleFrom: new Date('2026-01-26T18:00:00'),
   },
 ];
