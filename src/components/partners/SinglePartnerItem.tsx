3e89564ae9efe7e8b3232dc866c7f1d8a625be6f
Pavos21 <metalmasterboy@gmail.com>
2026-04-13 12:35:35 +0200

    Logo and color fix

--- src/components/partners/SinglePartnerItem.tsx
+++ src/components/partners/SinglePartnerItem.tsx
@@ -8,7 +8,7 @@ type Props = {
 export const SinglePartnerItem = ({ headline, img, alt, link }: Props) => {
   return (
     <div className="space-y-4">
-      {headline && <h4 className="text-blue text-h2 text-center">{headline}</h4>}
+      {headline && <h4 className="text-violet text-h2 text-center">{headline}</h4>}
       <ExternalLink
         icon={<img src={img} alt={alt} width={400} height={300} className="h-[90px] object-contain" loading="lazy" />}
         link={link}
