72e8be45ffba83723b12fbf609e2011d91ae3179
Pavos21 <metalmasterboy@gmail.com>
2026-05-04 16:16:02 +0200

    Mapa

--- src/components/lineup/MapSection.tsx
+++ src/components/lineup/MapSection.tsx
@@ -8,7 +8,7 @@ import { MajalesAreaMap } from '../svg/MajalesAreaMap';
 
 export const MapSection = () => {
   return (
-    <SectionContainer id="mapa" bgColor="lightpink">
+    <SectionContainer id="mapa" bgColor="lightOrange">
       <div className="pt-16 md:pt-20 px-4 md:px-16 flex justify-center items-center">
         <div className="w-full max-w-[600px]">
           <img src={mapa} alt="Majáles Area Map" className="w-full h-auto" />
@@ -21,21 +21,20 @@ export const MapSection = () => {
       {/* TOP LEFT */}
       <div className="absolute top-0 left-0">
         <div className="relative">
-          <Triangle className="absolute top-24 md:top-48 left-0 md:left-0 h-12 md:h-24" color='#E7548A' />
-          <CheckerSquare className="absolute top-12 md:top-24 left-0 md:left-0 h-12 md:h-24" color1='#000000' color2='#FFFFFF' />
-          <TextRectangle className="absolute top-0 md:top-0 left-0 md:left-0 h-12 md:h-24 w-48 md:w-96 overflow-y-hidden" color='#6459FD'>
-            <h2 className="text-h2-reversed-mobile md:text-h2-reversed text-white">
+          <Triangle className="absolute top-12 md:top-24 left-0 md:left-0 h-12 md:h-24" colorMain='fill-violet' />
+          <TextRectangle className="absolute top-0 md:top-0 left-0 md:left-0 h-12 md:h-24 w-48 md:w-96 overflow-y-hidden bg-orange" color=''>
+            <h2 className="text-2xl md:text-5xl font-medium text-beige">
               Mapa areálu
             </h2>
           </TextRectangle>           
-          <Triangle className="absolute top-0 md:top-0 left-48 md:left-96 h-12 md:h-24" color='#FFFFFF' />
+          <Triangle className="absolute top-0 md:top-0 left-48 md:left-96 h-12 md:h-24" colorMain='fill-orange' />
         </div>
       </div>
 
       {/* BOTTOM RIGHT */}
       <div className="absolute bottom-0 right-0">
         <div className="relative">
-          <Triangle className="absolute -top-12 md:-top-24 right-0 md:right-0 h-12 md:h-24 rotate-180" color='#1F2041' />
+          <Triangle className="absolute -top-12 md:-top-24 right-0 md:right-0 h-12 md:h-24 rotate-180" colorMain='fill-beige' />
         </div>
       </div>
     </SectionContainer>
